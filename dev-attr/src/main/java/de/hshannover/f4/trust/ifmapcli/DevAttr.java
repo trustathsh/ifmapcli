e8e440048d68336f110eb5acc4f78b7953510789
Ralf Steuerwald <ralf.steuerwald@hs-hannover.de>
2013-12-18 13:03:59 +0100

    Update license header and template

--- dev-attr/src/main/java/de/hshannover/f4/trust/ifmapcli/DevAttr.java
+++ dev-attr/src/main/java/de/hshannover/f4/trust/ifmapcli/DevAttr.java
@@ -18,7 +18,7 @@
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de
  * 
- * This file is part of Ifmapcli, version 0.0.6, implemented by the Trust@HsH
+ * This file is part of ifmapcli (dev-attr), version 0.0.6, implemented by the Trust@HsH
  * research group at the Hochschule Hannover.
  * %%
  * Copyright (C) 2010 - 2013 Trust@HsH
