e5a3f12b81744c109ab99ba0bb82f323296d808f
Ralf Steuerwald <ralf.steuerwald@hs-hannover.de>
2014-01-30 14:08:51 +0100

    ReqInv: improve argument parsing and delete filter

--- req-inv/src/main/java/de/hshannover/f4/trust/ifmapcli/ReqInv.java
+++ req-inv/src/main/java/de/hshannover/f4/trust/ifmapcli/ReqInv.java
@@ -7,17 +7,17 @@
  *    | | | |  | |_| \__ \ |_| | (_| |  _  |\__ \|  _  |
  *    |_| |_|   \__,_|___/\__|\ \__,_|_| |_||___/|_| |_|
  *                             \____/
- * 
+ *
  * =====================================================
- * 
+ *
  * Hochschule Hannover
  * (University of Applied Sciences and Arts, Hannover)
  * Faculty IV, Dept. of Computer Science
  * Ricklinger Stadtweg 118, 30459 Hannover, Germany
- * 
+ *
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de
- * 
+ *
  * This file is part of ifmapcli (req-inv), version 0.0.6, implemented by the Trust@HsH
  * research group at the Hochschule Hannover.
  * %%
@@ -26,9 +26,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,22 +38,23 @@
  */
 package de.hshannover.f4.trust.ifmapcli;
 
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import javax.net.ssl.TrustManager;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 import org.w3c.dom.Document;
 
 import de.hshannover.f4.trust.ifmapcli.common.Common;
-import de.hshannover.f4.trust.ifmapcli.common.Config;
+import de.hshannover.f4.trust.ifmapcli.common.ParserUtil;
 import de.hshannover.f4.trust.ifmapj.IfmapJ;
 import de.hshannover.f4.trust.ifmapj.IfmapJHelper;
 import de.hshannover.f4.trust.ifmapj.binding.IfmapStrings;
 import de.hshannover.f4.trust.ifmapj.channel.SSRC;
-import de.hshannover.f4.trust.ifmapj.exception.IfmapErrorResult;
-import de.hshannover.f4.trust.ifmapj.exception.IfmapException;
-import de.hshannover.f4.trust.ifmapj.exception.InitializationException;
 import de.hshannover.f4.trust.ifmapj.identifier.Identifier;
 import de.hshannover.f4.trust.ifmapj.identifier.Identifiers;
 import de.hshannover.f4.trust.ifmapj.messages.MetadataLifetime;
@@ -71,96 +72,117 @@ import de.hshannover.f4.trust.ifmapj.metadata.StandardIfmapMetadataFactory;
  *
  */
 public class ReqInv {
-	final static String CMD = "req-inv";
-	final static int MIN_ARGS = 4;			// update|delete, dev, ip
-	final static int EXPECTED_ARGS = 9;		// update|delete, dev, ip,
-											// url, user, pass,
-											// keystorePath, keystorePass
+
+	enum IdType {
+		ipv4, ipv6, mac
+	}
+
+	public final static String CMD = "req-inv";
 
 	// in order to create the necessary objects, make use of the appropriate
 	// factory classes
 	private static StandardIfmapMetadataFactory mf = IfmapJ
 			.createStandardMetadataFactory();
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		String op, dev, ip, qualifier;
-		Config cfg;
-		SSRC ssrc;
-		PublishRequest req;
-		PublishUpdate publishUpdate;
-		PublishDelete publishDelete;
-		TrustManager[] tms;
-		Identifier devIdentifier;
-		Identifier ipIdentifier;
-		Document metadata;
-		InputStream is;
+		final String KEY_OPERATION = "publishOperation";
+		final String KEY_DEVICE = "device";
+		final String KEY_OTHER_IDENTIFIER_TYPE = "other-identifier-type";
+		final String KEY_OTHER_IDENTIFIER = "other-identifier";
+		final String KEY_QUALIFIER = "qualifier";
 
-		// check number of mandatory command line arguments
-		if(args.length < MIN_ARGS){
-			ReqInv.usage();
-			return;
-		}
+		ArgumentParser parser = ArgumentParsers.newArgumentParser(CMD);
+		parser.addArgument("publish-operation")
+			.type(String.class)
+			.dest(KEY_OPERATION)
+			.choices("update", "delete")
+			.help("the publish operation");
+		parser.addArgument(KEY_DEVICE)
+			.type(String.class)
+			.dest(KEY_DEVICE)
+			.help("the name of the device identifier");
+		parser.addArgument(KEY_OTHER_IDENTIFIER_TYPE)
+			.type(IdType.class)
+			.dest(KEY_OTHER_IDENTIFIER_TYPE)
+			.choices(IdType.ipv4, IdType.ipv6, IdType.mac)
+			.help("the type of the other identifier");
+		parser.addArgument(KEY_OTHER_IDENTIFIER)
+			.type(String.class)
+			.dest(KEY_OTHER_IDENTIFIER)
+			.help("the name of the other identifier");
+		parser.addArgument("--qualifier")
+			.type(String.class)
+			.dest(KEY_QUALIFIER)
+			.help("the qualifier for the request-for-investigation");
+		ParserUtil.addConnectionArgumentsTo(parser);
+		ParserUtil.addCommonArgumentsTo(parser);
 
-		// parse mandatory command line arguments
-		op = args[0];
-		dev = args[1];
-		ip = args[2];
-		qualifier = args[3];
-		if(Common.isUpdateorDelete(op) == false){
-			ReqInv.usage();
-			return;
-		}
-
-		// check and load optional parameters
-		cfg = Common.checkAndLoadParams(args, EXPECTED_ARGS);
-		System.out.println(CMD + " uses config " + cfg);
-
-		// prepare identifiers
-		devIdentifier = Identifiers.createDev(dev);
-		ipIdentifier = Identifiers.createIp4(ip);
-
-		// prepare metadata
-		metadata = mf.createRequestForInvestigation(qualifier);
-
-		// update or delete
-		if(Common.isUpdate(op)){
-			publishUpdate = Requests.createPublishUpdate(devIdentifier, ipIdentifier,
-					metadata, MetadataLifetime.forever);
-			req = Requests.createPublishReq(publishUpdate);
-		} else {
-			String filter = "meta:request-for-investigation";
-			publishDelete = Requests.createPublishDelete(devIdentifier, ipIdentifier, filter);
-			publishDelete.addNamespaceDeclaration(IfmapStrings.STD_METADATA_PREFIX,
-					IfmapStrings.STD_METADATA_NS_URI);
-			req = Requests.createPublishReq(publishDelete);
-		}
-
-		// publish
+		Namespace res = null;
 		try {
-			is = Common.prepareTruststoreIs(cfg.getTruststorePath());
-			tms = IfmapJHelper.getTrustManagers(is, cfg.getTruststorePass());
-			ssrc = IfmapJ.createSSRC(cfg.getUrl(), cfg.getUser(), cfg.getPass(), tms);
+			res = parser.parseArgs(args);
+		} catch (ArgumentParserException e) {
+			parser.handleError(e);
+			System.exit(1);
+		}
+
+		Identifier deviceIdentifier = Identifiers.createDev(res.getString(KEY_DEVICE));
+		IdType otherIdentifierType = res.get(KEY_OTHER_IDENTIFIER_TYPE);
+		Identifier otherIdentifier = getIdentifier(
+				otherIdentifierType,
+				res.getString(KEY_OTHER_IDENTIFIER));
+		String qualifier = (res.getString(KEY_QUALIFIER) == null) ? "" : res.getString(KEY_QUALIFIER);
+		Document metadata = mf.createRequestForInvestigation(qualifier);
+
+		SSRC ssrc = null;
+		try {
+			InputStream is = Common.prepareTruststoreIs(res.getString(ParserUtil.KEYSTORE_PATH));
+			TrustManager[] tms = IfmapJHelper.getTrustManagers(is, res.getString(ParserUtil.KEYSTORE_PASS));
+			ssrc = IfmapJ.createSSRC(
+					res.getString(ParserUtil.URL),
+					res.getString(ParserUtil.USER),
+					res.getString(ParserUtil.PASS),
+					tms);
 			ssrc.newSession();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		PublishRequest req = Requests.createPublishReq();
+
+		if (res.getString(KEY_OPERATION).equals("update")) {
+			PublishUpdate publishUpdate = Requests.createPublishUpdate(
+					deviceIdentifier, otherIdentifier, metadata, MetadataLifetime.forever);
+			req.addPublishElement(publishUpdate);
+		} else if (res.getString(KEY_OPERATION).equals("delete")) {
+			String filter = String.format(
+				"meta:request-for-investigation[@ifmap-publisher-id='%s' and @qualifier='%s']",
+				ssrc.getPublisherId(), qualifier);
+			PublishDelete publishDelete = Requests.createPublishDelete(
+					deviceIdentifier, otherIdentifier, filter);
+			publishDelete.addNamespaceDeclaration("meta", IfmapStrings.STD_METADATA_NS_URI);
+			req.addPublishElement(publishDelete);
+		}
+
+		try {
 			ssrc.publish(req);
 			ssrc.endSession();
-		} catch (InitializationException e) {
-			System.out.println(e.getDescription() + " " + e.getMessage());
-		} catch (IfmapErrorResult e) {
-			System.out.println(e.getErrorString());
-		} catch (IfmapException e) {
-			System.out.println(e.getDescription() + " " + e.getMessage());
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
 		}
 	}
 
-	private static void usage() {
-		System.out.println("usage:\n" +
-				"\t" + ReqInv.CMD + " update|delete dev ip qualifier" +
-				"[url user pass truststore truststorePass]");
-		System.out.println(Common.USAGE);
+	private static final Identifier getIdentifier(IdType type, String name) {
+		switch (type) {
+		case ipv4:
+			return Identifiers.createIp4(name);
+		case ipv6:
+			return Identifiers.createIp6(name);
+		case mac:
+			return Identifiers.createMac(name);
+		default:
+			throw new RuntimeException("unknown identifier type '" + type + "'");
+		}
 	}
 }
